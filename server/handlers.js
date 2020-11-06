82251349569988407a05167da0ea25f7548c6914
Alex Langevin <alex_langevin@hotmail.ca>
2020-11-06 15:17:34 -0500

    Exercice 4 and 5 completed

--- server/handlers.js
+++ server/handlers.js
@@ -42,24 +42,21 @@ const bookSeats = async (req, res) => {
     //All the info about the purchase
     const {seatId, fullName, email, creditCard, expiration} = req.body;
     if (!(fullName || email || creditCard || expiration)) {
-      res.status(404).json({ status: 404, message: "Missing information fields in the form" });
+      res.status(404).json({ status: 404, message: 'Missing information fields in the form' });
     } else {
       const query = { _id: seatId };
-
       await client.connect();
-
-      const db = client.db("seats-db");
-      
-      const selectedSeat = await db.collection("seats").findOne({ _id: seatId });
+      const db = client.db('seats-db');
+      const selectedSeat = await db.collection('seats').findOne({ _id: seatId });
 
       // check if seat isBooked or not
       let seat
       if (selectedSeat.isBooked) {
-        seat = await db.collection("seats").updateOne(query, { 
+        seat = await db.collection('seats').updateOne(query, { 
           $set: { isBooked: false } 
         });
       } else {
-        seat = await db.collection("seats").updateOne(query, {
+        seat = await db.collection('seats').updateOne(query, {
           $set: {
             isBooked: true,
             customer: {
@@ -81,4 +78,64 @@ const bookSeats = async (req, res) => {
   client.close();
 };
 
-module.exports = { getSeats, bookSeats };
+const deleteReservation = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  const {seatId} = req.body;
+  const query = { '_id': seatId };
+
+  try {
+    await client.connect();
+    const db = client.db('seats-db');
+    let seat = await db.collection('seats').updateOne(query, { 
+      $set: { 
+        isBooked: false,
+        customer: {
+          "fullName":"",
+          "email":"",
+        },
+    }
+  });
+    assert.equal(1, seat.matchedCount);
+    assert.equal(1, seat.modifiedCount);
+    res.status(204).json({ status: 204, success: true });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({status: 500, message: err.message});
+  }
+  client.close();
+};
+
+const updateCustomer = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  const {seatId, fullName, email} = req.body;
+  const query = { '_id': seatId };
+
+  try {
+    await client.connect();
+    const db = client.db('seats-db');
+    const selectedSeat = await db.collection('seats').findOne(query);
+
+    let seat;
+      if (selectedSeat.isBooked) {
+        seat = await db.collection('seats').updateOne(query, { 
+          $set: {
+            isBooked: true,
+            customer: {
+              fullName,
+              email,
+            },
+          },
+        });
+        assert.equal(1, seat.matchedCount);
+        assert.equal(1, seat.modifiedCount);
+      }
+
+    res.status(204).json({ status: 204, success: true });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({status: 500, message: err.message});
+  }
+  client.close();
+};
+
+module.exports = { getSeats, bookSeats, deleteReservation, updateCustomer };
--- server/routes.js
+++ server/routes.js
@@ -1,49 +1,12 @@
 const router = require("express").Router();
 
-const {getSeats, bookSeats} = require('./handlers');
+const {getSeats, bookSeats, deleteReservation, updateCustomer} = require('./handlers');
 
-//MongoDB stuff
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const assert = require('assert');
-const { MONGO_URI } = process.env;
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-//Seats constants
-const NUM_OF_ROWS = 8;
-const SEATS_PER_ROW = 12;
-
-//////// HELPERS
-const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
-
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
-
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
-    const seatId = `${getRowName(row)}-${seat + 1}`;
-
-    bookedSeats[seatId] = true;
-
-    num--;
-  }
-
-  return bookedSeats;
-};
-
-let state;
 
 router.get("/api/seat-availability", getSeats);
-
-let lastBookingAttemptSucceeded = false;
-
 router.post("/api/book-seat", bookSeats);
+router.post('/api/deleteReservation', deleteReservation);
+router.post('/api/updateCustomer', updateCustomer);
 
 module.exports = router;
